1e25655a70c243d6fb83b400a661f071b5ac0671
Abhinav Abhi Verma <abhinavbb77@gmail.com>
2019-05-13 23:53:06 +0530

    Initial COMMIT

---
+++ src/pack1/Test1.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void Login() {
+		Reporter.log("1", true);
+
+	}
+
+}
---
+++ src/pack1/Test2.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void login() {
+
+		Reporter.log("2", true);
+	}
+
+}
